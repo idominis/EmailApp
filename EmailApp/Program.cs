a98c90d8954fc4ffbeeb0ceea8b92ccdd3254996
Ivan Dominis <ivan.dominis@emovis.hr>
2024-06-05 21:59:28 +0200

    - Serilog added - Logging added to EmailService

--- EmailApp/Program.cs
+++ EmailApp/Program.cs
@@ -4,6 +4,8 @@ using EmailApp.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,10 +15,25 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<IEmailService, EmailService>();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (connectionString == null)
+{
+    throw new InvalidOperationException("Could not find a connection string named 'DefaultConnection'.");
+}
 builder.Services.AddDbContext<EmailDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.Console()
+    //.WriteTo.File("C:\\Logs\\EmailApp.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
