c1c5b69d467d0a15f8afdd00db04b99c79636623
Ivan Dominis <ivan.dominis@emovis.hr>
2024-06-05 13:36:28 +0200

    - Pagination - Email detasils Modal, jQuerry added

--- EmailApp/wwwroot/js/interop.js
+++ EmailApp/wwwroot/js/interop.js
@@ -1,3 +1,21 @@
 ﻿window.confirmCancel = function (message) {
     return confirm(message);
 };
+
+window.showModal = (modalId) => {
+    if (window.jQuery) {
+        $(`#${modalId}`).modal('show');
+    } else {
+        console.error("jQuery is not loaded");
+    }
+};
+
+window.hideModal = (modalId) => {
+    if (window.jQuery) {
+        $(`#${modalId}`).modal('hide');
+    } else {
+        console.error("jQuery is not loaded");
+    }
+};
+
+
